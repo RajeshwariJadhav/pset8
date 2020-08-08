92dd96c13832bf82aa8792d49c6ba26b938b5806
Rajeshwari Jadhav <44329307+RajeshwariJadhav@users.noreply.github.com>
2020-08-08 20:35:05 +0530

    Updated and working version

--- p5/normGN.py
+++ p5/normGN.py
@@ -1,38 +1,7 @@
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
-# Start with random A having independent columns, and set X accordingly
-
-# at every iteration:
-# 1. h = minimize L1(x+dx) + Lambda (w(h)-r) --> 
-# 2. add ax to A and find x accordingly
-
-S = 4
-M = 3
-w = 4
-h = 5
-
-A = np.array([
-    [1,0,0],
-    [0,1,0],
-    [0,0,1],
-    [0,1,0]
-])
-# 4 x 3
-
-y = np.array([
-    [0, 1, 0, 6, 3, 0, 6, 0, 7, 8, 0, 0, 0, 3, 0, 1 , 0, 1, 3, 0],
-    [0, 4, 7, 0, 0, 1, 2, 3, 6, 8, 5, 5, 8, 4, 7, 3 , 7, 0, 3, 4],
-    [1, 5, 4, 3, 3, 0, 6, 0, 7, 3, 0, 1, 0, 3, 0, 1 , 4, 0, 0, 0],
-    [1, 1, 2, 3, 4, 5, 6, 1, 7, 6, 8, 1, 9, 3, 5, 0 , 4, 0, 3, 2]
-])
-# 4 x 20
-
-X = np.dot(np.linalg.pinv(A),y)
-# 3 x 20
-
-A = A.astype(float)
-X = X.astype(float)
+import random
 
 
 def psi(h, A, X):
@@ -40,6 +9,9 @@ def psi(h, A, X):
     dx = h[1]
     ret = np.array([np.dot(A, dx) + np.dot(da, X), np.diag(np.dot(A.T, da))])
     return ret
+
+def r(da, dx, hnew, h):
+    r = r + psi(hnew)-psi(h)
 
 def J(h, nu):
     da = h[0]
@@ -50,69 +22,103 @@ def J(h, nu):
         ret = np.random.random()/10000
         ret += temp
         return ret
-    return np.inf
-    
-def finalCost(y,A,X):
-    return np.linalg.norm(y - np.dot(A, X))
-
-def projection(A, X):
-    for i in range(len(A[0])):
-        A[:,i] = A[:,i]/np.linalg.norm(A[:,i])
-    X = X + np.dot(np.linalg.pinv(A),(y-np.dot(A, X)))
-    return A, X
-
-def hCostOld(x, r, lambd, A, X):
-    p = psi(x, A, X)
-    p[0] = np.linalg.norm(p[0]-r[0])
-    p[1] = np.linalg.norm(p[1]-r[1]) 
-    return J(x, np.inf) + lambd*p
-
+    return np.inf 
 
 def hCost(x, r, lambd, A, Q):
     p = psi(x, A, Q)
     p[0] = np.linalg.norm(p[0]-r[0])
     p[1] = np.linalg.norm(p[1]-r[1])
-    ret = J(x, np.inf) + np.linalg.norm(lambd*p)
+    jval = J(x, np.inf) 
+    pval = np.sqrt(np.linalg.norm(p[0]-r[0])**2+np.linalg.norm(p[1]-r[1])**2)
+    ret = jval + lambd*pval
     return ret
 
-def findH(h, r, lambd):
+def findH(h, r, lambd, A, X):
     x0 = h
     optimalH = opt.minimize(hCost, x0, args = (r, lambd, A, X), method = "CG")
     return (optimalH.x)
 
-def r(da, dx, hnew, h):
-    r = r + psi(hnew)-psi(h)
-
-    
-def GN(A, X):
-    maxIters = 100
-    i = 0
-    h = [np.random.random(A.shape), np.random.random(X.shape)]
-    r = np.array([np.zeros(np.dot(A,X).shape),np.zeros(M)])
-    lambd = 1
-    iters = list()
-    costs = list()
-    while i < maxIters:
-        newh = findH(h, r, lambd)
-        r = r + psi(newh, A, X)-psi(h, A, X)
-        h = newh
-        A += h[0]
-        A, X = projection(A,X)
-        A = np.nan_to_num(A)
-        X = np.nan_to_num(X)
-        i+=1
-        iters.append(i)
-        costs.append(finalCost(y,A,X))
-    plt.plot(iters, costs)
+def projection(A, X):
+    #unit columns for A
+    for i in range(len(A[0])):
+        A[:,i] = A[:,i]/np.linalg.norm(A[:,i])
+    #Frobenius norm for X    
+    X = X + np.dot(np.linalg.pinv(A),(y-np.dot(A, X)))
     return A, X
 
+def finalCost(h, r, lambd, A,X,dx, y):
+    ret = hCost(h, r, lambd, A, X)
+    return ret
+    
+def GN(A, X):
+    #initialize h to zeros
+    h = [np.zeros(A.shape), np.zeros(X.shape)]
+    r = np.array([np.zeros(np.dot(A,X).shape),np.zeros(M)])
+    
+    lambd = 0.01
+    iters = list()
+    costs = list()
+    
+    i = 0
+    maxIters = 100
+    while i < maxIters:
+        #find the optimal h = [da, dx] that minimizes hCost
+        newh = findH(h, r, lambd, A, X)
+        #update r
+        r = r + psi(newh, A, X)-psi(h, A, X)
+        #update h
+        h = newh
+        #update A and X according da and dx
+        A += h[0]
+        X += h[1]
+        #project at every iter
+        A, X = projection(A,X)
+        i+=1
+    return A, X
 
-r = np.zeros(M)
-x0 = np.array([np.random.random(A.shape), np.random.random(X.shape)])
+S = 4
+M = 3
+w = 4
+h = 5
+g = 0
 
-A, X = GN(A, X)
-print("A ", A)
-print("X", X)
-print("AX", np.dot(A,X))
-print("y", y)
+A = np.array([
+    [1,0,0],
+    [0,1,0],
+    [0,0,1],
+    [0,1,0]
+    ]) 
+
+X = np.array([
+    [0, 3, 0, 0, 0, 0, 6, 0, 7, 0, 0, 0, 0, 3, 0, 0 , 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 5, 0, 0, 0, 0 , 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 , 0, 0, 0, 0]
+])
+
+y = np.dot(A,X)
+
+while g<2:
+    A = np.array([
+    [0,0,1],
+    [1,0,0],
+    [0,1,0],
+    [0,1,0]
+    ])    #different from the A used to find y
+
+    A = np.array([
+    [0.245,0,0],
+    [0,0.874,0],
+    [0,0,0.598],
+    [0,0,0]
+    ])    
+        
+    X = np.dot(np.linalg.pinv(A),y) #initialize X according to A
+    X = X.astype(float)
+    A, X = GN(A, X)
+    print(np.linalg.norm(y-np.dot(A,X)))
+    print("A ", A)
+    print("X", X.round(1))
+    print("AX", np.dot(A,X).round(3))
+    print("y", y)
+    g+=1
 
